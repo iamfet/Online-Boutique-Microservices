bfda1938122427dd2ab9b275fd44fc8ddc438bed
iamfet <fetuyee@gmail.com>
2025-05-31 14:39:57 +0000

    refactor(terraform): move variable values to tfvars

--- infra/modules/variables.tf
+++ infra/modules/variables.tf
@@ -1,43 +1,31 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
 }
 
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.30"
 }
 
 variable "node_groups" {
@@ -51,15 +39,4 @@ variable "node_groups" {
       min_size     = number
     })
   }))
-  default = {
-    general = {
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
-      scaling_config = {
-        desired_size = 2
-        max_size     = 4
-        min_size     = 1
-      }
-    }
-  }
 }
